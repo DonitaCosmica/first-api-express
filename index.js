3f00527e6f22f57c4b92dc890f5763ffe483c00e
DonitaCosmica <donnetpitalua127@gmail.com>
2023-08-27 22:37:30 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,120 @@
+const express = require('express')
+const crypto = require('node:crypto')
+const cors = require('cors')
+
+const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors(
+  {
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        'http://localhost:8080',
+        'http://localhost:1234',
+        'https://movies.com',
+        'https://midu.dev'
+      ]
+
+      if(ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true)
+      }
+
+      if(!origin) {
+        return callback(null, true)
+      }
+
+      return callback(new Error('Not Found by CORS'))
+    }
+  }
+))
+
+app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+  res.json({ message: 'hola mundo' })
+})
+
+app.get('/movies', (req, res) => {
+  //res.header('Access-Control-Allow-Origin', '*')
+
+  const { genre } = req.query
+  if(genre) {
+    const filterMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filterMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if(movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie Not Found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if(result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if(movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie Not Found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if(!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if(movieIndex === -1){
+    return res.status(404).json({ message: 'Movie Not Found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
